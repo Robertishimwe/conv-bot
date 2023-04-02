27f5c32ebd4c5cb4cab711afeb9a9de9ab5719fa
Robertishimwe <81079680+Robertishimwe@users.noreply.github.com>
2023-04-02 14:15:33 +0000

    test

---
+++ model/conversation.js
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+const ConversationSchema = new Schema({
+    // user: {
+    //     type: mongoose.Schema.Types.ObjectId,
+    //     ref: 'User'
+    // },
+   
+
+    user: {
+        type: String
+    },
+
+    message: {
+        type: String 
+    },
+
+    CreatedDate: {
+        type: Date,
+        default: Date.now(),
+    },
+});
+
+
+const Conversation = mongoose.model('Conversation', ConversationSchema);
+
+export default Conversation;
