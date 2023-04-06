000f6bdb9777ac4f275a8c714dbbce4b1793a520
Robertishimwe <81079680+Robertishimwe@users.noreply.github.com>
2023-04-06 11:52:51 +0000

    googleAuth

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-
+import session from 'express-session';
 import routes from './routes/index.js';
 import connectdb from './config/database.js';
 
@@ -8,6 +8,11 @@ import connectdb from './config/database.js';
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use(session({
+    secret: 'melody hensley is my spirit animal',
+    resave: true,
+    saveUninitialized: true
+  }));
 
 app.use('/api', routes);
 
