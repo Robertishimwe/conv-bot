da57bb603c35557f3af65ab006f1856a6371d6d3
Robertishimwe <81079680+Robertishimwe@users.noreply.github.com>
2023-04-06 10:33:24 +0000

    added schema

---
+++ model/user.js
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+	userName: {
+		type: String,
+		required: true,
+		max: 30,
+		min: 3,
+	},
+	email: {
+		type: String,
+		max: 30,
+		min: 3,
+	},
+
+    profilePicture:{
+        type: String
+    },
+	Role: {
+		type: String,
+		enum: ['basic_user','premium_user', 'admin'],
+		required: true,
+	},
+	CreatedDate: {
+		type: Date,
+		default: Date.now(),
+	},
+});
+
+const User = mongoose.model('User', UserSchema);
+
+export default User;
