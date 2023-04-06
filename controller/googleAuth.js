e885553bfcccda2a0283a716c166e5098cc8c397
Robertishimwe <81079680+Robertishimwe@users.noreply.github.com>
2023-04-06 11:59:44 +0000

    googleAuth

--- controller/googleAuth.js
+++ controller/googleAuth.js
@@ -13,13 +13,14 @@ const { generateToken } = Token;
 class googleController {
 	static onSuccess = async (req, res) => {
 		try {
-			const { name, id, email, displayName } = req.user;
+			const { name, id, email, displayName,profile } = req.user;
 
 			const newUser = {
 				userName: displayName,
 				googleId: id,
 				Role: 'client',
 				email,
+				profile
 			};
 
 			console.log(newUser)
