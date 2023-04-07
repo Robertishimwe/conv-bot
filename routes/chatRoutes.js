31e5bae1df5a28baea4128110c588a524a1dbe03
Robertishimwe <81079680+Robertishimwe@users.noreply.github.com>
2023-04-07 11:19:56 +0000

    op

--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import verify from '../middleware/verify';
+import verify from '../middleware/verify.js';
 
 import chatControllers from '../controller/chatController.js'
 
